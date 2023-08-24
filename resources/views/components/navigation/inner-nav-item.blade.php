58ad9b031e0b7e4384f636da0ad5e9aac3a6c847
Randall Wilk <wilk.randall@gmail.com>
2023-08-24 10:35:39 -0500

    wip

--- resources/views/components/navigation/inner-nav-item.blade.php
+++ resources/views/components/navigation/inner-nav-item.blade.php
@@ -3,20 +3,14 @@
        {{ $attributes->class($linkClass())->style($linkStyles()) }}
     >
         @if ($icon)
-            @if ($filament)
-                <x-filament::icon
-                    :icon="$icon"
-                    :class="$iconClass()"
-                />
-            @else
-                <x-dynamic-component
-                    :component="$icon"
-                    :class="$iconClass()"
-                    aria-hidden="true"
-                />
-            @endif
-
-            <span class="truncate w-full hover:text-clip hover:whitespace-normal">{{ $slot }}</span>
+            <x-dynamic-component
+                :component="$filament ? 'filament::icon' : $icon"
+                :icon="$icon"
+                :class="$iconClass()"
+                aria-hidden="true"
+            />
         @endif
+
+        <span class="truncate w-full hover:text-clip hover:whitespace-normal">{{ $slot }}</span>
     </a>
 </li>
